3552d61dd8b0bcccf7cb54f8445e1e94f58df4a8
Rodney Lab <codesign@rodneylab.com>
2022-08-04 12:00:45 +0100

    fix: 💫 update for SvelteKit breaking changes

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { imagetools } from 'vite-imagetools';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  define: {
+    'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
+  },
+  plugins: [sveltekit(), vanillaExtractPlugin(), imagetools({ force: true })],
+  ssr:
+    process.env.NODE_ENV === 'development'
+      ? {}
+      : {
+          noExternal: ['@vanilla-extract/css', '@vanilla-extract/css/fileScope'],
+        },
+};
+
+export default config;
