253399a2fabf9ac3e17a6f82817828b471d3accb
Rodney Lab <codesign@rodneylab.com>
2022-08-20 17:50:08 +0100

    style: 🛁  linting

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { theme } from '$lib/shared/stores/theme';
+	import theme from '$lib/shared/stores/theme';
 	import { summerTheme } from '$lib/styles/themes/summerTheme.css';
 	import { winterTheme } from '$lib/styles/themes/winterTheme.css';
 	import { container, content } from './Card.css';
--- src/lib/shared/stores/theme.ts
+++ src/lib/shared/stores/theme.ts
@@ -4,7 +4,7 @@ import { writable } from 'svelte/store';
 const defaultValue = 'summer';
 const initialValue = browser ? window.localStorage.getItem('theme') ?? defaultValue : defaultValue;
 
-export const theme = writable<string>(initialValue);
+const theme = writable<string>(initialValue);
 
 theme.subscribe((value) => {
 	if (browser) {
@@ -12,4 +12,4 @@ theme.subscribe((value) => {
 	}
 });
 
-export { theme as default };
+export default theme;
